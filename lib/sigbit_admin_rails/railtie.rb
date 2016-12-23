c53809b17b4d56456eafb45394879a62e7e68d84
Ante Wall <antewall@gmail.com>
2016-12-23 10:37:43 +0100

    Add page_header helper

--- lib/sigbit_admin_rails/railtie.rb
+++ lib/sigbit_admin_rails/railtie.rb
@@ -1,11 +1,13 @@
 require 'sigbit_admin_rails/helpers/application_helper'
 require 'sigbit_admin_rails/helpers/sidebar_helper'
+require 'sigbit_admin_rails/helpers/page_header_helper'
 
 module SigbitAdminRails
   class Railtie < Rails::Railtie
     initializer 'sigbit_admin_rails.view_helpers' do
       ActionView::Base.send :include, ApplicationHelper
       ActionView::Base.send :include, SidebarHelper
+      ActionView::Base.send :include, PageHeaderHelper
     end
   end
 end
