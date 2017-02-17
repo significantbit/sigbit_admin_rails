a39d3dae0248191752ea4a40bc3d94ba14b5bdee
Johan André <johan@significantbit.se>
2017-02-17 22:00:28 +0100

    Reducing method size

--- lib/sigbit_admin_rails/helpers/sidebar_helper.rb
+++ lib/sigbit_admin_rails/helpers/sidebar_helper.rb
@@ -16,25 +16,24 @@ module SigbitAdminRails
     def sidebar_item(title, url = nil, &block)
       if block_given?
         url = "##{generate_random_id}" unless url.present?
-
         content_tag :li do
-          concat(link_to("#{title} #{dropdown_arrow_icon}".html_safe, url, data: { toggle: 'collapse' }))
+          concat(link_to("#{title} #{dropdown_arrow_icon}".html_safe, url, data: {toggle: 'collapse'}))
           concat(content_tag(:ul, class: 'submenu collapse', id: url.delete('#')) do
             yield
           end)
         end
       else
-        content_tag :li, class: 'nav-item' do
-          smart_link_to title, url, class: 'nav-link'
-        end
+        smart_link_to title, url, class: 'nav-link'
       end
     end
 
     def smart_link_to(title, url, options = {})
-      if defined?(ActiveLinkTo)
-        active_link_to(title, url, options)
-      else
-        link_to title, url, options
+      content_tag :li, class: 'nav-item' do
+        if defined?(ActiveLinkTo)
+          active_link_to(title, url, options)
+        else
+          link_to title, url, options
+        end
       end
     end
 
