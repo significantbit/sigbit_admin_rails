6a535b0083032f6958766b3bda5365b1e9d6ed92
Johan André <johan@significantbit.se>
2017-02-17 19:54:26 +0100

    - Using active_link_to if defined (falling back to link_to) - Cleaning up concat of html (no longer need html_safe on generated output) - Removing inline html (except the icon chevron) - When a block is passed to sidebar_item (for generating a dropdown), an random id is created

--- lib/sigbit_admin_rails/helpers/sidebar_helper.rb
+++ lib/sigbit_admin_rails/helpers/sidebar_helper.rb
@@ -13,17 +13,29 @@ module SigbitAdminRails
       end
     end
 
-    def sidebar_item(title, url, &block)
+    def sidebar_item(title, url = nil, &block)
       if block_given?
+        url = "##{(0...20).map { ('a'..'z').to_a[rand(26)] }.join}" unless url.present?
+
         content_tag :li do
-          html = link_to("#{title} <i class='fa fa-chevron-right'></i>".html_safe, url, data: { toggle: 'collapse' })
-          html << content_tag(:ul, class: 'submenu collapse', id: url.delete('#')) do
+          concat(link_to("#{title} <i class='fa fa-chevron-right'></i>".html_safe, url, data: {toggle: 'collapse'}))
+          concat(content_tag(:ul, class: 'submenu collapse', id: url.delete('#')) do
             yield
-          end
-          html.html_safe
+          end)
         end
       else
-        "<li>#{link_to title, url}</li>".html_safe
+        content_tag :li, class: 'nav-item' do
+          smart_link_to title, url, class: 'nav-link'
+        end
       end
-    end  end
+    end
+
+    def smart_link_to(title, url, options = {})
+      if defined?(ActiveLinkTo)
+        active_link_to(title, url, options)
+      else
+        link_to title, url, options
+      end
+    end
+  end
 end
